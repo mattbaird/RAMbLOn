4fa6d918aeee065c5fb0b55c98f256e4e455f1d5
Matthew Baird <mattbaird@gmail.com>
2016-10-28 13:13:19 -0700

    add support for generating docs instead of previewing.

--- ramblon.go
+++ ramblon.go
@@ -9,6 +9,7 @@ import (
 	"github.com/martini-contrib/render"
 	"github.com/satori/go.uuid"
 	"github.com/tsaikd/KDGoLib/jsonex"
+	"gopkg.in/urfave/cli.v1"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -35,9 +36,313 @@ func main() {
 		os.Exit(-1)
 	}
 	log.Printf("configuration is:%v\n", configuration)
+	var outputdir string
+	var zipOutput bool
+	var isLocal bool
+	app := cli.NewApp()
+	app.HelpName = "RamblOn"
+	app.Usage = "Generate or Live Preview HTML based API Documentation from RAML v1.0"
+	app.Version = "0.0.1"
+	app.Commands = []cli.Command{
+		{
+			Name:    "Preview",
+			Aliases: []string{"p", "preview"},
+			Usage:   "Live Preview HTML based documentation from RAML v1.0",
+			Action: func(c *cli.Context) error {
+				m := martini.Classic()
+				m.Use(martini.Static("public")) // serve from the "static" directory
+				templateFuncs := getTemplateFuncs()
 
-	m := martini.Classic()
-	m.Use(martini.Static("public")) // serve from the "static" directory
+				m.Use(render.Renderer(render.Options{
+					Directory: "templates", // Specify what path to load the templates from.
+					//		Layout:          "layout",                       // Specify a layout template. Layouts can call {{ yield }} to render the current template.
+					Extensions:      []string{".tmpl"},                 // Specify extensions to load for templates.
+					Funcs:           []template.FuncMap{templateFuncs}, // Specify helper function maps for templates to access.
+					Delims:          render.Delims{"{{", "}}"},         // Sets delimiters to the specified strings.
+					Charset:         "UTF-8",                           // Sets encoding for json and html content-types. Default is "UTF-8".
+					IndentJSON:      true,                              // Output human readable JSON
+					IndentXML:       true,                              // Output human readable XML
+					HTMLContentType: "text/html",                       // Output XHTML content type instead of default "text/html"
+				}))
+
+				m.Get("/", func(r render.Render) {
+					ramlFile := "/home/matthew/code/go/src/github.com/AtScaleInc/modeler/api-docs/api.raml"
+					log.Printf("parsing:%v\n", ramlFile)
+					var checkRAMLVersion bool
+					var allowIntToBeNum bool
+					var checkOptions = []parser.CheckValueOption{}
+					var err error
+
+					ramlParser := parser.NewParser()
+
+					if allowIntToBeNum {
+						checkOptions = append(checkOptions, parser.CheckValueOptionAllowIntegerToBeNumber(true))
+					}
+
+					if err = ramlParser.Config(parserConfig.CheckRAMLVersion, checkRAMLVersion); err != nil {
+						pretty.Printf("error during config[CheckRAMLVersion]:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+
+					if err = ramlParser.Config(parserConfig.CheckValueOptions, checkOptions); err != nil {
+						pretty.Printf("error during config[CheckValueOptions]:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+
+					rootdoc, err := ramlParser.ParseFile(ramlFile)
+					if err != nil {
+						pretty.Printf("error during ParseFile:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+
+					jsonBytes, err := jsonex.MarshalIndent(rootdoc, "", "  ")
+					if err != nil {
+						pretty.Printf("error during MarshalIndent:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+					if false {
+						log.Printf("json:%v\n", string(jsonBytes))
+					}
+					r.HTML(200, "index", rootdoc)
+				})
+
+				m.Get("/browse", func(r render.Render, params martini.Params) {
+					files, err := ioutil.ReadDir(configuration.Root)
+					if err != nil {
+						log.Printf("error:%v\n", err)
+					}
+					var directories []os.FileInfo
+					for _, file := range files {
+						if file.IsDir() {
+							directories = append(directories, file)
+						}
+					}
+					r.HTML(200, "browse", directories)
+				})
+
+				m.Get("/browse/:directory", func(r render.Render, params martini.Params) {
+					directory := params["directory"]
+					files, err := ioutil.ReadDir(fmt.Sprintf("%s%s", configuration.Root, directory))
+					if err != nil {
+						log.Printf("error:%v\n", err)
+					}
+					var apis []os.FileInfo
+					for _, file := range files {
+						if !file.IsDir() {
+							apis = append(apis, file)
+						}
+					}
+					r.HTML(200, "browseapi", map[string]interface{}{
+						"directory": directory,
+						"apis":      apis})
+				})
+
+				m.NotFound(func(r render.Render) {
+					r.HTML(404, "404", nil)
+				})
+
+				m.Get("/api/:directory/:name", func(r render.Render, params martini.Params) {
+					directory := params["directory"]
+					name := params["name"]
+					fullDirectory, err := getDirectory(directory)
+					if err != nil {
+						pretty.Printf("error during PWD lookup:%v\n", err)
+						r.HTML(500, "500", err)
+						return
+					}
+					ramlFile := fmt.Sprintf("%s/%s", fullDirectory, name)
+					log.Printf("file is:%s\n", ramlFile)
+					project := Project{directory: fullDirectory}
+					projects.projects = append(projects.projects, &project)
+					err = watch(fullDirectory, name)
+					if err != nil {
+						pretty.Printf("error during File Watch:%v\n", err)
+						r.HTML(500, "500", err)
+						return
+					}
+					var checkRAMLVersion bool
+					var allowIntToBeNum bool
+					var checkOptions = []parser.CheckValueOption{}
+
+					ramlParser := parser.NewParser()
+
+					if allowIntToBeNum {
+						checkOptions = append(checkOptions, parser.CheckValueOptionAllowIntegerToBeNumber(true))
+					}
+
+					if err = ramlParser.Config(parserConfig.CheckRAMLVersion, checkRAMLVersion); err != nil {
+						pretty.Printf("error during config[CheckRAMLVersion]:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+
+					if err = ramlParser.Config(parserConfig.CheckValueOptions, checkOptions); err != nil {
+						pretty.Printf("error during config[CheckValueOptions]:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+
+					rootdoc, err := ramlParser.ParseFile(ramlFile)
+					if err != nil {
+						pretty.Printf("error during ParseFile:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+
+					jsonBytes, err := jsonex.MarshalIndent(rootdoc, "", "  ")
+					if err != nil {
+						pretty.Printf("error during MarshalIndent:%v", err)
+						r.HTML(500, "500", err)
+						return
+					}
+					if false {
+						log.Printf("json:%v\n", string(jsonBytes))
+					}
+					u1 := uuid.NewV4()
+					clientUUID := u1.String()
+					r.HTML(200, "index", map[string]interface{}{
+						"directory":  directory,
+						"name":       name,
+						"raml":       rootdoc,
+						"prefix":     "",
+						"clientUUID": clientUUID})
+				})
+
+				// This is the sockets connection for the project, it is a json mapping to sockets.
+				m.Get("/sockets/projects/:directory/:project/:clientUUID", sockets.JSON(Message{}), func(params martini.Params,
+					receiver <-chan *Message, sender chan<- *Message, done <-chan bool, disconnect chan<- int, err <-chan error) (int, string) {
+					log.Printf("**** called\n")
+					client := &Client{params["clientUUID"], receiver, sender, done, err, disconnect}
+					directory, e := getDirectory(params["directory"])
+					if e != nil {
+						pretty.Printf("error during File Watch:%v\n", err)
+						return 500, e.Error()
+					}
+					projects := projects.getProject(directory)
+					projects.appendClient(client)
+
+					// A single select can be used to do all the messaging
+					for {
+						select {
+						case <-client.err:
+							// Don't try to do this:
+							// client.out <- &Message{"system", "system", "There has been an error with your connection"}
+							// The socket connection is already long gone.
+							// Use the error for statistics etc
+						case msg := <-client.in:
+							log.Printf("client message:%v\n", msg)
+						case <-client.done:
+							projects.removeClient(client)
+							return 200, "OK"
+						}
+					}
+				})
+				m.RunOnAddr(fmt.Sprintf(":%v", configuration.Port))
+				return nil
+			},
+		},
+		{
+			Name:    "Generate",
+			Aliases: []string{"g", "generate"},
+			Usage:   "Generate HTML based documentation from RAML v1.0",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:        "outputdir, o",
+					Value:       "./tmp",
+					Usage:       "Directory to output generated documentation to.",
+					Destination: &outputdir,
+				},
+				cli.BoolFlag{
+					Name:        "zip,z",
+					Usage:       "Create ZIP of Documentation",
+					Destination: &zipOutput,
+				},
+				cli.BoolFlag{
+					Name:        "local,l",
+					Usage:       "If serving from local files vs. http server",
+					Destination: &isLocal,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				outputdir := c.String("outputdir")
+				err := os.MkdirAll(outputdir, os.ModeDir|0777)
+				if err != nil {
+					pretty.Printf("Error Creating Output Directory [%s]: %v\n", outputdir, err)
+					return nil
+				}
+				filename := "index.html"
+				fullPathAndDir := fmt.Sprintf("%s/%s", outputdir, filename)
+				if strings.HasSuffix(outputdir, "/") {
+					fullPathAndDir = fmt.Sprintf("%s%s", outputdir, filename)
+				}
+				f, err := os.Create(fullPathAndDir)
+				if err != nil {
+					pretty.Printf("Error Creating file [%s]: %v\n", fullPathAndDir, err)
+					return nil
+				}
+				defer f.Close()
+				t := template.Must(template.New("index.tmpl").Funcs(getTemplateFuncs()).ParseFiles("./templates/index.tmpl"))
+
+				var checkRAMLVersion bool
+				var allowIntToBeNum bool
+				var checkOptions = []parser.CheckValueOption{}
+
+				ramlParser := parser.NewParser()
+
+				if allowIntToBeNum {
+					checkOptions = append(checkOptions, parser.CheckValueOptionAllowIntegerToBeNumber(true))
+				}
+
+				if err = ramlParser.Config(parserConfig.CheckRAMLVersion, checkRAMLVersion); err != nil {
+					pretty.Printf("error during config[CheckRAMLVersion]:%v", err)
+					return err
+				}
+
+				if err = ramlParser.Config(parserConfig.CheckValueOptions, checkOptions); err != nil {
+					pretty.Printf("error during config[CheckValueOptions]:%v", err)
+					return err
+				}
+				ramlFile := "/home/matthew/code/go/src/github.com/AtScaleInc/modeler/api-docs/api.raml"
+
+				rootdoc, err := ramlParser.ParseFile(ramlFile)
+				if err != nil {
+					pretty.Printf("error during ParseFile:%v", err)
+					return err
+				}
+				prefix := ""
+				if isLocal {
+					prefix = "."
+				}
+				stuff := map[string]interface{}{
+					"directory":  "/home/matthew/code/go/src/github.com/AtScaleInc/modeler/api-docs/",
+					"name":       rootdoc.Name,
+					"raml":       rootdoc,
+					"prefix":     prefix,
+					"clientUUID": "na"}
+
+				err = t.Execute(f, stuff)
+				if err != nil {
+					pretty.Printf("Error executing template: %v\n", err)
+					return err
+				}
+				// now copy the directories (CSS and IMAGES)
+				err = CopyDir("./public/", "./tmp/")
+				if err != nil {
+					pretty.Printf("Error copying static files: %v\n", err)
+					return err
+				}
+				return nil
+			},
+		},
+	}
+	app.Run(os.Args)
+}
+
+func getTemplateFuncs() map[string]interface{} {
 	var templateFuncs map[string]interface{} = make(map[string]interface{})
 	templateFuncs["safe"] = func(s string) template.HTML { return template.HTML(s) }
 	templateFuncs["underscore"] = func(s string) string {
@@ -58,195 +363,7 @@ func main() {
 			return s
 		}
 	}
-
-	m.Use(render.Renderer(render.Options{
-		Directory: "templates", // Specify what path to load the templates from.
-		//		Layout:          "layout",                       // Specify a layout template. Layouts can call {{ yield }} to render the current template.
-		Extensions:      []string{".tmpl"},                 // Specify extensions to load for templates.
-		Funcs:           []template.FuncMap{templateFuncs}, // Specify helper function maps for templates to access.
-		Delims:          render.Delims{"{{", "}}"},         // Sets delimiters to the specified strings.
-		Charset:         "UTF-8",                           // Sets encoding for json and html content-types. Default is "UTF-8".
-		IndentJSON:      true,                              // Output human readable JSON
-		IndentXML:       true,                              // Output human readable XML
-		HTMLContentType: "text/html",                       // Output XHTML content type instead of default "text/html"
-	}))
-
-	m.Get("/", func(r render.Render) {
-		ramlFile := "/home/matthew/code/go/src/github.com/AtScaleInc/modeler/api-docs/api.raml"
-		log.Printf("parsing:%v\n", ramlFile)
-		var checkRAMLVersion bool
-		var allowIntToBeNum bool
-		var checkOptions = []parser.CheckValueOption{}
-		var err error
-
-		ramlParser := parser.NewParser()
-
-		if allowIntToBeNum {
-			checkOptions = append(checkOptions, parser.CheckValueOptionAllowIntegerToBeNumber(true))
-		}
-
-		if err = ramlParser.Config(parserConfig.CheckRAMLVersion, checkRAMLVersion); err != nil {
-			pretty.Printf("error during config[CheckRAMLVersion]:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-
-		if err = ramlParser.Config(parserConfig.CheckValueOptions, checkOptions); err != nil {
-			pretty.Printf("error during config[CheckValueOptions]:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-
-		rootdoc, err := ramlParser.ParseFile(ramlFile)
-		if err != nil {
-			pretty.Printf("error during ParseFile:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-
-		jsonBytes, err := jsonex.MarshalIndent(rootdoc, "", "  ")
-		if err != nil {
-			pretty.Printf("error during MarshalIndent:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-		if false {
-			log.Printf("json:%v\n", string(jsonBytes))
-		}
-		r.HTML(200, "index", rootdoc)
-	})
-
-	m.Get("/browse", func(r render.Render, params martini.Params) {
-		files, err := ioutil.ReadDir(configuration.Root)
-		if err != nil {
-			log.Printf("error:%v\n", err)
-		}
-		var directories []os.FileInfo
-		for _, file := range files {
-			if file.IsDir() {
-				directories = append(directories, file)
-			}
-		}
-		r.HTML(200, "browse", directories)
-	})
-
-	m.Get("/browse/:directory", func(r render.Render, params martini.Params) {
-		directory := params["directory"]
-		files, err := ioutil.ReadDir(fmt.Sprintf("%s%s", configuration.Root, directory))
-		if err != nil {
-			log.Printf("error:%v\n", err)
-		}
-		var apis []os.FileInfo
-		for _, file := range files {
-			if !file.IsDir() {
-				apis = append(apis, file)
-			}
-		}
-		r.HTML(200, "browseapi", map[string]interface{}{
-			"directory": directory,
-			"apis":      apis})
-	})
-
-	m.NotFound(func(r render.Render) {
-		r.HTML(404, "404", nil)
-	})
-
-	m.Get("/api/:directory/:name", func(r render.Render, params martini.Params) {
-		directory := params["directory"]
-		name := params["name"]
-		fullDirectory, err := getDirectory(directory)
-		if err != nil {
-			pretty.Printf("error during PWD lookup:%v\n", err)
-			r.HTML(500, "500", err)
-			return
-		}
-		ramlFile := fmt.Sprintf("%s/%s", fullDirectory, name)
-		log.Printf("file is:%s\n", ramlFile)
-		project := Project{directory: fullDirectory}
-		projects.projects = append(projects.projects, &project)
-		err = watch(fullDirectory, name)
-		if err != nil {
-			pretty.Printf("error during File Watch:%v\n", err)
-			r.HTML(500, "500", err)
-			return
-		}
-		var checkRAMLVersion bool
-		var allowIntToBeNum bool
-		var checkOptions = []parser.CheckValueOption{}
-
-		ramlParser := parser.NewParser()
-
-		if allowIntToBeNum {
-			checkOptions = append(checkOptions, parser.CheckValueOptionAllowIntegerToBeNumber(true))
-		}
-
-		if err = ramlParser.Config(parserConfig.CheckRAMLVersion, checkRAMLVersion); err != nil {
-			pretty.Printf("error during config[CheckRAMLVersion]:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-
-		if err = ramlParser.Config(parserConfig.CheckValueOptions, checkOptions); err != nil {
-			pretty.Printf("error during config[CheckValueOptions]:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-
-		rootdoc, err := ramlParser.ParseFile(ramlFile)
-		if err != nil {
-			pretty.Printf("error during ParseFile:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-
-		jsonBytes, err := jsonex.MarshalIndent(rootdoc, "", "  ")
-		if err != nil {
-			pretty.Printf("error during MarshalIndent:%v", err)
-			r.HTML(500, "500", err)
-			return
-		}
-		if false {
-			log.Printf("json:%v\n", string(jsonBytes))
-		}
-		u1 := uuid.NewV4()
-		clientUUID := u1.String()
-		r.HTML(200, "index", map[string]interface{}{
-			"directory":  directory,
-			"name":       name,
-			"raml":       rootdoc,
-			"clientUUID": clientUUID})
-	})
-
-	// This is the sockets connection for the project, it is a json mapping to sockets.
-	m.Get("/sockets/projects/:directory/:project/:clientUUID", sockets.JSON(Message{}), func(params martini.Params,
-		receiver <-chan *Message, sender chan<- *Message, done <-chan bool, disconnect chan<- int, err <-chan error) (int, string) {
-		log.Printf("**** called\n")
-		client := &Client{params["clientUUID"], receiver, sender, done, err, disconnect}
-		directory, e := getDirectory(params["directory"])
-		if e != nil {
-			pretty.Printf("error during File Watch:%v\n", err)
-			return 500, e.Error()
-		}
-		projects := projects.getProject(directory)
-		projects.appendClient(client)
-
-		// A single select can be used to do all the messaging
-		for {
-			select {
-			case <-client.err:
-				// Don't try to do this:
-				// client.out <- &Message{"system", "system", "There has been an error with your connection"}
-				// The socket connection is already long gone.
-				// Use the error for statistics etc
-			case msg := <-client.in:
-				log.Printf("client message:%v\n", msg)
-			case <-client.done:
-				projects.removeClient(client)
-				return 200, "OK"
-			}
-		}
-	})
-	m.RunOnAddr(fmt.Sprintf(":%v", configuration.Port))
+	return templateFuncs
 }
 
 func getDirectory(directory string) (string, error) {
